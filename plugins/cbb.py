2ba4ed40d1187fd370d0b96d4537ed5ce08fc1bb
beeresh1234 <139480918+beeresh1234@users.noreply.github.com>
2026-05-17 12:11:16 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -42,8 +42,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text=START_MSG.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help'),
-                 InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data='about')]
+                [InlineKeyboardButton("⚡ ᴄʜᴀɴɴᴇʟꜱ ⚡", callback_data='help'),
+                 InlineKeyboardButton("⭕ ᴀʙᴏᴜᴛ ⭕", callback_data='about')]
             ])
         )
 
